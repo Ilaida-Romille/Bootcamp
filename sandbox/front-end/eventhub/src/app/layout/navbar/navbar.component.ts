2a368393b96bb74daa941c9fe71425c0f6a696fa
Romille Ilaida <romille.ilaida@pointwest.com>
2026-07-24 09:25:21 +0800

    Migrated Login and Platform Dashboard to Angular

---
+++ sandbox/front-end/eventhub/src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent { }
