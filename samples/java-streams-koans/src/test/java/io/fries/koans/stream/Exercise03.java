3749ee08dcb6cf68039b1f0c4eceb0fd758ee2c3
Romille Ilaida <romille.ilaida@pointwest.com>
2026-08-11 09:09:46 +0800

    chore: complete java streams koans

---
+++ samples/java-streams-koans/src/test/java/io/fries/koans/stream/Exercise03.java
@@ -0,0 +1,56 @@
+package io.fries.koans.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Exercise03 extends OnlineStore {
+
+    /**
+     * Count how many items there are in {@code Customer.wantsToBuy} using {@link Stream#count}.
+     */
+    @Test
+    void how_many_items_wanted() {
+        List<Customer> customerList = mall.getCustomers();
+
+        long sum = customerList.stream()
+                .flatMap(customer -> customer.getWantsToBuy().stream())
+                .count();
+
+        assertThat(sum).isEqualTo(32L);
+    }
+
+    /**
+     * Find the richest customer's budget by using {@link Stream#max} and {@link Comparator#naturalOrder}.
+     * Don't use {@link Stream#sorted}.
+     */
+    @Test
+    void richest_customer() {
+        List<Customer> customerList = mall.getCustomers();
+
+        Comparator<Integer> comparator = Comparator.naturalOrder();
+        Optional<Integer> richestCustomer = customerList.stream().map(Customer::getBudget).max(comparator);
+
+        assertThat(comparator.getClass().getSimpleName()).isEqualTo("NaturalOrderComparator");
+        assertThat(richestCustomer.get()).isEqualTo(12000);
+    }
+
+    /**
+     * Find the youngest customer by using {@link Stream#min}.
+     * Don't use {@link Stream#sorted}.
+     */
+    @Test
+    void youngest_customer() {
+        List<Customer> customerList = mall.getCustomers();
+
+        Comparator<Customer> comparator = Comparator.comparing(Customer::getAge);
+        Optional<Customer> youngestCustomer = customerList.stream().min(comparator);
+
+        assertThat(youngestCustomer.get()).isEqualTo(customerList.get(8));
+    }
+}
---
+++ samples/java-streams-koans/src/test/java/io/fries/koans/stream/Exercise04.java
@@ -0,0 +1,63 @@
+package io.fries.koans.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Exercise04 extends OnlineStore {
+
+    /**
+     * Find the first customer who registered this online store by using {@link Stream#findFirst}.
+     * The customerList are ascending ordered by registered timing.
+     */
+    @Test
+    void first_registrant() {
+        List<Customer> customerList = mall.getCustomers();
+
+        Optional<Customer> firstCustomer = customerList.stream().findFirst();
+
+        assertThat(firstCustomer.get()).isEqualTo(customerList.get(0));
+    }
+
+    /**
+     * Check whether any customer older than 40 exists or not, by using {@link Stream#anyMatch}.
+     */
+    @Test
+    void is_there_anyone_older_than_40() {
+        List<Customer> customerList = mall.getCustomers();
+
+        boolean olderThan40Exists = customerList.stream().anyMatch(customer -> customer.getAge() > 40);
+
+        assertThat(olderThan40Exists).isFalse();
+    }
+
+    /**
+     * Check whether all customer are older than 20 or not, by using {@link Stream#allMatch}.
+     */
+    @Test
+    void is_everybody_older_than_20() {
+        List<Customer> customerList = mall.getCustomers();
+
+        boolean allOlderThan20 = customerList.stream().allMatch(customer -> customer.getAge() > 20);
+
+        assertThat(allOlderThan20).isTrue();
+    }
+
+    /**
+     * Confirm that none of the customer has empty list for their {@code Customer.wantsToBuy}
+     * by using {@link Stream#noneMatch}.
+     */
+    @Test
+    void everyone_wants_something() {
+        List<Customer> customerList = mall.getCustomers();
+
+        boolean everyoneWantsSomething = customerList.stream()
+                .noneMatch(customer -> customer.getWantsToBuy().isEmpty());
+
+        assertThat(everyoneWantsSomething).isTrue();
+    }
+}
