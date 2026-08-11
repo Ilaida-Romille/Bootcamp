9fbe53419e54c5997f495dd0a6809b9cd0f645f8
Romille Ilaida <romille.ilaida@pointwest.com>
2026-08-11 12:54:03 +0800

    chore: configure h2-console

--- sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/EventhubriApplication.java
+++ sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/EventhubriApplication.java
@@ -1,7 +1,11 @@
 package com.pointwest.bootcamp.eventhubri;
 
+import jakarta.servlet.Servlet;
+import org.h2.server.web.JakartaWebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventhubriApplication {
@@ -10,4 +14,11 @@ public class EventhubriApplication {
 		SpringApplication.run(EventhubriApplication.class, args);
 	}
 
+	@Bean
+    public ServletRegistrationBean<Servlet> h2servletRegistration() {
+        // Uses JakartaWebServlet for Spring Boot 3+ / Jakarta EE compatibility
+        ServletRegistrationBean<Servlet> registration = new ServletRegistrationBean<>(new JakartaWebServlet());
+        registration.addUrlMappings("/h2-console/*");
+        return registration;
+    }
 }
