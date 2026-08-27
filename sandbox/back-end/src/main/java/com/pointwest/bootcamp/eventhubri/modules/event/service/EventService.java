b160597e05c19bda848e19e804478849a7c8ac5b
Romille Ilaida <romille.ilaida@pointwest.com>
2026-08-28 01:09:09 +0800

    feat(event): create event service

---
+++ sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/event/service/EventService.java
@@ -0,0 +1,30 @@
+package com.pointwest.bootcamp.eventhubri.modules.event.service;
+
+import com.pointwest.bootcamp.eventhubri.modules.event.dto.EventCreateRequestDto;
+import com.pointwest.bootcamp.eventhubri.modules.event.dto.EventResponseDto;
+import com.pointwest.bootcamp.eventhubri.modules.event.dto.EventUpdateRequestDto;
+
+import java.util.List;
+
+public interface EventService {
+
+    EventResponseDto createEvent(
+            EventCreateRequestDto request,
+            String authenticatedUserEmail);
+
+    List<EventResponseDto> getAllEvents(
+            String authenticatedUserEmail);
+
+    EventResponseDto getEventById(
+            Long eventId,
+            String authenticatedUserEmail);
+
+    EventResponseDto updateEvent(
+            Long eventId,
+            EventUpdateRequestDto request,
+            String authenticatedUserEmail);
+
+    void deleteEvent(
+            Long eventId,
+            String authenticatedUserEmail);
+}
