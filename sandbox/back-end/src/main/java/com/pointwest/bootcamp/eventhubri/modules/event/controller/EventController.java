899048cbe81bb17ed147f8ccf3a965fd15441dae
Romille Ilaida <romille.ilaida@pointwest.com>
2026-08-28 16:46:11 +0800

    refactor(event-controller): pre-authorize controllers

--- sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/event/controller/EventController.java
+++ sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/event/controller/EventController.java
@@ -7,6 +7,7 @@ import com.pointwest.bootcamp.eventhubri.modules.event.service.EventService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,7 @@ public class EventController {
     private final EventService eventService;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('MANAGE_EVENTS')")
     public ResponseEntity<EventResponseDto> createEvent(
             @Valid @RequestBody EventCreateRequestDto request,
             Authentication authentication) {
@@ -36,6 +38,7 @@ public class EventController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('VIEW_ALL_ORGANIZATIONS')")
     public ResponseEntity<List<EventResponseDto>> getAllEvents(
             Authentication authentication) {
 
@@ -45,6 +48,7 @@ public class EventController {
     }
 
     @GetMapping("/{eventId}")
+    @PreAuthorize("hasAuthority('MANAGE_AGENDA')")
     public ResponseEntity<EventResponseDto> getEventById(
             @PathVariable Long eventId,
             Authentication authentication) {
