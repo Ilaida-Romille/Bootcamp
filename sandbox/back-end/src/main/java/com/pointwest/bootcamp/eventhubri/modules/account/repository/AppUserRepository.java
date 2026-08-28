56df86ba7c0dc84b4a768e7bfcf76c854a07ae1b
Romille Ilaida <romille.ilaida@pointwest.com>
2026-08-28 16:44:38 +0800

    refactor(auth-repo): add new query method

--- sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/account/repository/AppUserRepository.java
+++ sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/account/repository/AppUserRepository.java
@@ -13,4 +13,6 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmailIgnoreCase(String email);
 
     boolean existsByEmailIgnoreCase(String email);
+
+    Optional<AppUser> findById(Long id);
 }
