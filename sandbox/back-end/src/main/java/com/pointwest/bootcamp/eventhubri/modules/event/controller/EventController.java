2e4b0b7ad257fd61f54e29d49a3748cb9d509932
Romille Ilaida <romille.ilaida@pointwest.com>
2026-08-31 10:11:59 +0800

    refactor(event-controller): add endpoint for paginated requests and queries

--- sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/event/controller/EventController.java
+++ sandbox/back-end/src/main/java/com/pointwest/bootcamp/eventhubri/modules/event/controller/EventController.java
@@ -3,13 +3,19 @@ package com.pointwest.bootcamp.eventhubri.modules.event.controller;
 import com.pointwest.bootcamp.eventhubri.modules.event.dto.EventCreateRequestDto;
 import com.pointwest.bootcamp.eventhubri.modules.event.dto.EventResponseDto;
 import com.pointwest.bootcamp.eventhubri.modules.event.dto.EventUpdateRequestDto;
+import com.pointwest.bootcamp.eventhubri.modules.event.entity.Event;
 import com.pointwest.bootcamp.eventhubri.modules.event.service.EventService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Sort;
 
 import java.net.URI;
 import java.util.List;
@@ -19,68 +25,89 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EventController {
 
-    private final EventService eventService;
+        private final EventService eventService;
 
-    @PostMapping
-    @PreAuthorize("hasAuthority('MANAGE_EVENTS')")
-    public ResponseEntity<EventResponseDto> createEvent(
-            @Valid @RequestBody EventCreateRequestDto request,
-            Authentication authentication) {
+        @PostMapping
+        @PreAuthorize("hasAuthority('MANAGE_EVENTS')")
+        public ResponseEntity<EventResponseDto> createEvent(
+                        @Valid @RequestBody EventCreateRequestDto request,
+                        Authentication authentication) {
 
-        EventResponseDto response = eventService.createEvent(
-                request,
-                authentication.getName());
+                EventResponseDto response = eventService.createEvent(
+                                request,
+                                authentication.getName());
 
-        return ResponseEntity
-                .created(
-                        URI.create("/api/events/" + response.id()))
-                .body(response);
-    }
+                return ResponseEntity
+                                .created(
+                                                URI.create("/api/events/" + response.id()))
+                                .body(response);
+        }
 
-    @GetMapping
-    @PreAuthorize("hasAuthority('VIEW_ALL_ORGANIZATIONS')")
-    public ResponseEntity<List<EventResponseDto>> getAllEvents(
-            Authentication authentication) {
+        // @GetMapping
+        // @PreAuthorize("hasAuthority('VIEW_ALL_ORGANIZATIONS')")
+        // public ResponseEntity<List<EventResponseDto>> getAllEvents(
+        // Authentication authentication) {
 
-        return ResponseEntity.ok(
-                eventService.getAllEvents(
-                        authentication.getName()));
-    }
+        // return ResponseEntity.ok(
+        // eventService.getAllEvents(
+        // authentication.getName()));
+        // }
 
-    @GetMapping("/{eventId}")
-    @PreAuthorize("hasAuthority('MANAGE_AGENDA')")
-    public ResponseEntity<EventResponseDto> getEventById(
-            @PathVariable Long eventId,
-            Authentication authentication) {
+        @GetMapping("/{eventId}")
+        @PreAuthorize("hasAuthority('MANAGE_AGENDA')")
+        public ResponseEntity<EventResponseDto> getEventById(
+                        @PathVariable Long eventId,
+                        Authentication authentication) {
 
-        return ResponseEntity.ok(
-                eventService.getEventById(
-                        eventId,
-                        authentication.getName()));
-    }
+                return ResponseEntity.ok(
+                                eventService.getEventById(
+                                                eventId,
+                                                authentication.getName()));
+        }
 
-    @PutMapping("/{eventId}")
-    public ResponseEntity<EventResponseDto> updateEvent(
-            @PathVariable Long eventId,
-            @Valid @RequestBody EventUpdateRequestDto request,
-            Authentication authentication) {
+        // Pagination:
 
-        return ResponseEntity.ok(
-                eventService.updateEvent(
-                        eventId,
-                        request,
-                        authentication.getName()));
-    }
+        @GetMapping
+        @PreAuthorize("hasAuthority('MANAGE_EVENTS')")
+        public ResponseEntity<Page<EventResponseDto>> getEvents(
+                        @RequestParam(required = false) Event.Status status,
+                        @RequestParam(required = false) Event.EventType eventType,
+                        @RequestParam(required = false) String search,
+                        @PageableDefault(size = 10, sort = "startTime", direction = Sort.Direction.DESC) Pageable pageable,
+                        Authentication authentication) {
 
-    @DeleteMapping("/{eventId}")
-    public ResponseEntity<Void> deleteEvent(
-            @PathVariable Long eventId,
-            Authentication authentication) {
+                Page<EventResponseDto> response = eventService.getOrganizationEventsPaginated(
+                                status,
+                                eventType,
+                                search,
+                                pageable,
+                                authentication.getName());
 
-        eventService.deleteEvent(
-                eventId,
-                authentication.getName());
+                return ResponseEntity.ok(response);
+        }
 
-        return ResponseEntity.noContent().build();
-    }
+        @PutMapping("/{eventId}")
+        public ResponseEntity<EventResponseDto> updateEvent(
+                        @PathVariable Long eventId,
+                        @Valid @RequestBody EventUpdateRequestDto request,
+                        Authentication authentication) {
+
+                return ResponseEntity.ok(
+                                eventService.updateEvent(
+                                                eventId,
+                                                request,
+                                                authentication.getName()));
+        }
+
+        @DeleteMapping("/{eventId}")
+        public ResponseEntity<Void> deleteEvent(
+                        @PathVariable Long eventId,
+                        Authentication authentication) {
+
+                eventService.deleteEvent(
+                                eventId,
+                                authentication.getName());
+
+                return ResponseEntity.noContent().build();
+        }
 }
